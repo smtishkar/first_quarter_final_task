6dcc390322df66daef7fcaf5bfe0fd71302d8524
Sergey <smtishkar@mail.ru>
2022-10-11 10:22:00 +0300

    Добавил создание и печать массива

---
+++ Final_task/Program.cs
@@ -0,0 +1,23 @@
+﻿int size = 3;
+string[] FillArray(int size)
+{
+    string[] arr = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write("Введите число или слово: ");
+        string value = Console.ReadLine();
+        arr[i] = value;
+    }
+    return arr;
+}
+
+void PrintArray(string[] array)
+{
+    int count = size;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
